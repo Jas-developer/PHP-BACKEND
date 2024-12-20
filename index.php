d0b3767e9a78d2dad4739d9cab0d7fa01dfbc755
= <=>
2024-12-19 19:52:39 -0500

    learning for each loop

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
 <?php 
 
-//II am currently learning php
-$i = 0;
-
-while($i < 6){
-    $i++;
-    if($i == 2) continue;
-     echo "Looping {$i} <br/>";
+//php for loop
+for($x = 0; $x <= 10; $x++){
+    if($x == 3)continue;
+    echo "The number is: $x <br>";
 }
 
-// The while loop will continue the execution as long as the value is true
-//continue statement will skipp the specified condition 
-//break statement will stop the loop
+//php for each loop
 
+//this is an array 
+$colors = array("red","blue","yellow");
 
-
-
+//this is a for each loop in an array
+foreach($colors as $x){
+    echo "$x <br>";
+}
 
 
 
